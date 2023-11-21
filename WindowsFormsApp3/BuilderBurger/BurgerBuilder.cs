1f03b83419b1ff9bf1bb3d096c207b9f38870b68
315 <315@315-DE-8>
2023-11-21 15:40:23 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp3/BuilderBurger/BurgerBuilder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WindowsFormsApp3.DBCon;
+
+namespace WindowsFormsApp3.BuilderBurger
+{
+    public class BurgerBuilder : IBurgerBuilder
+    {
+        private Burgers _burger;
+
+        public BurgerBuilder()
+        {
+            _burger = new Burgers();
+        }
+        public Burgers GetBurgers()
+        {
+            Burgers burger = _burger;
+            _burger = new Burgers();
+            return burger;
+        }
+
+        IBurgerBuilder IBurgerBuilder.AddBacon()
+        {
+            _burger.Bacon = true; return this;
+        }
+
+        IBurgerBuilder IBurgerBuilder.AddCheese()
+        {
+            _burger.Cheese = true; return this;
+        }
+
+        IBurgerBuilder IBurgerBuilder.AddLetuce()
+        {
+            _burger.Letuce = true; return this;
+        }
+
+        IBurgerBuilder IBurgerBuilder.AddPickles()
+        {
+            _burger.Pickles = true; return this;
+        }
+
+        IBurgerBuilder IBurgerBuilder.AddTomato()
+        {
+            _burger.Tomato = true; return this;
+        }
+    }
+}
